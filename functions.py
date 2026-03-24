fa3816cedabe5dbefeabaeebd91c8df5d897358c
SHWETANSHUgithub <shwetanshusahu@gmail.com>
2026-03-24 22:56:41 +0530

    Add functions for sum, average, and area calculations

--- functions.py
+++ functions.py
@@ -1,1 +1,20 @@
+def sum(x,y):
+    return x+y
+print(sum(34,45))
+
+def average(a,b):
+    return (a+b)/2
+print(average(4,8))
+
+def areaoftriangle(x,z):
+    return 1/2*x*z
+print(areaoftriangle(45,56))
+
+def areaofsquare(x):
+    return x*x
+print(areaofsquare(6))
+
+def areaofrectangle(l,b,h):
+    return l*b*h
+print(areaofrectangle(56,87,76))
 
