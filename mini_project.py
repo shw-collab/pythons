9a247a0ca2bc6c086fd78fedb37716f4d9197547
SHWETANSHUgithub <shwetanshusahu@gmail.com>
2026-03-26 21:43:40 +0530

    Add basic calculator functionality in mini_project.py

---
+++ mini_project.py
@@ -0,0 +1,17 @@
+#mini project calculator
+first = int(input("enter the first number: "))
+operator = input("enter the operator(+,-,/,*,%): ")
+second = int(input("enter the second number: "))
+
+if operator == "+":
+    print(first+second)
+elif operator == "-":
+    print(first - second)
+elif operator == "/":
+    print(first/second)
+elif operator == "*":
+    print(first*second)
+elif operator == "%":
+    print(first%second)
+else:
+    print("error occured")
